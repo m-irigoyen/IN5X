d5e2702a1954edb7667a77be7def85263f092e67
Julien Robert <bob.julien020@gmail.com>
2016-01-03 23:16:53 +0100

    tg

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,22 @@ int main(int argc, char* argv[])
 {
 	// Création de la base de données
 	DatabaseHandler dbLearning, dbTest, dbAll;
-	DatabaseHandler::buildDatabase_allBlackFace(dbLearning, dbTest, dbAll);
+	DatabaseHandler::buildDatabase_all(dbLearning, dbTest, dbAll);
 
 	for (int i = 50; i < 600; i = i + 50) {
 		for (int j = 1; j < 3; ++j) {
 			// Construction des classes
 			ReconnaissanceHandler reconnaissance;
-			reconnaissance.buildClassesPCA(dbLearning, i, j);
-			//reconnaissance.buildClasses(dbLearning, i, j);
+			//reconnaissance.buildClassesPCA(dbLearning, i, j);
+			reconnaissance.buildClasses(dbLearning, i, j);
 
 			// Test pour une seule pièce
 			/*PIECE_TYPE resultat =  reconnaissance.completeReconaissance_one("n_pion_face (13)");
 			cout << "Ah ça, c'est un " << convert_pieceTypeToName(resultat) << endl;*/
 			// Test pour une db complète
 			vector<pair<DatabaseImageDescriptor, PIECE_TYPE>> results;
-			//reconnaissance.completeReconnaissance_db(dbTest, results, i, j);
-			reconnaissance.completeReconnaissance_dbPCA(dbTest, results, i, j);
+			reconnaissance.completeReconnaissance_db(dbTest, results, i, j);
+			//reconnaissance.completeReconnaissance_dbPCA(dbTest, results, i, j);
 			reconnaissance.analyseResults(results,i,j);
 		}
 	}
